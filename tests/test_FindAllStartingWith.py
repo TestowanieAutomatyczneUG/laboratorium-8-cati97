05d9ef09b1fd26a4ed57e303196b0798ccc620c7
unknown <katasiw97@gmail.com>
2020-11-24 18:20:13 +0100

    test_first_list_item_starts_wtih_a - green

--- tests/test_FindAllStartingWith.py
+++ tests/test_FindAllStartingWith.py
@@ -1,5 +1,6 @@
 import unittest
 from src.sample.FindAllStartingWith import *
+from assertpy import assert_that
 
 
 class TestSearchByName(unittest.TestCase):
@@ -7,6 +8,10 @@ class TestSearchByName(unittest.TestCase):
     def test_get_successful_status_code_200(self):
         self.search = FindAllStartingWith("a")
         self.assertEqual(self.search.get_status_code(), 200)
+
+    def test_first_list_item_starts_wtih_A(self):
+        self.search = FindAllStartingWith("a")
+        assert_that(self.search.get_list_of_names()[0]).starts_with("A")
 
 if __name__ == '__main__':
     unittest.main()
